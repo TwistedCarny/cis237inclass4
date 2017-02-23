0f3e0422db7f9083292888bfabb4a3755ad64ae8
Westin Curtis <westincurtis@gmail.com>
2017-02-22 20:02:11 -0500

    Everything done in class

---
+++ cis237inclass4/IIntegerLinkedList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237inclass4
+{
+    interface IIntegerLinkedList
+    {
+        void AddMaintainSort(int integerData);
+        int RemoveMaintainSort(int removeData);
+
+        void Display();
+
+        bool IsEmpty { get; }
+
+        int Size { get; }
+    }
+}
---
+++ cis237inclass4/IntegerLinkedList.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237inclass4
+{
+    class IntegerLinkedList : IIntegerLinkedList
+    {
+        //make node class
+        protected class Node
+        {
+            public int Data { get; set; }
+            public Node Next { get; set; }
+        }
+
+        // A couple of pointers to the head and tail of the linked list
+        protected Node _head;
+        protected Node _tail;
+        protected int _size;
+
+        public bool IsEmpty
+        {
+            get
+            {
+                return _head == null;
+            }
+        }
+
+        public int Size
+        {
+            get
+            {
+                return _size;
+            }
+        }
+
+        public void AddMaintainSort(int integerData)
+        {
+            Node newNode = new Node();
+            newNode.Data = integerData;
+
+            if(IsEmpty || _head.Data >= newNode.Data)
+            {
+                newNode.Next = _head;
+
+                _head = newNode;
+
+                if(_tail == null)
+                {
+                    _tail = _head;
+                }
+            }
+            else
+            {
+                Node currentNode = _head;
+
+                while(currentNode.Next != null && currentNode.Next.Data < newNode.Data)
+                {
+                    //Move down the list one node
+                    currentNode = currentNode.Next;
+                }
+
+                //Make the newNode's next property point to the current nodes next property.
+                newNode.Next = currentNode.Next;
+
+                currentNode.Next = newNode;
+
+                if(_tail == currentNode)
+                {
+                    _tail = newNode;
+                }
+            }
+            _size++;
+        }
+
+        public int RemoveMaintainSort(int removeData)
+        {
+            if (IsEmpty)
+            {
+                throw new Exception("List is empty");
+            }
+
+            int returnData;
+
+            if(_head == _tail || _head.Data == removeData)
+            {
+                returnData = _head.Data;
+
+                _head = _head.Next;
+
+                if(_head == null)
+                {
+                    _tail = null;
+                }
+            }
+            else
+            {
+                Node currentNode = _head;
+
+                while(currentNode.Next != _tail && currentNode.Next.Data != removeData)
+                {
+                    currentNode = currentNode.Next;
+                }
+
+                if(currentNode.Next.Data == removeData)
+                {
+                    returnData = currentNode.Next.Data;
+
+                    if(currentNode .Next == _tail)
+                    {
+                        _tail = currentNode;
+                    }
+
+                    currentNode.Next = currentNode.Next.Next;
+                }
+                else
+                {
+                    throw new Exception("Item not found");
+                }
+            }
+            _size--;
+
+            return returnData;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("The list is:");
+
+            Node currentNode = _head;
+
+            while(currentNode != null)
+            {
+                Console.WriteLine(currentNode.Data);
+                currentNode = currentNode.Next;
+            }
+        }
+    }
+
+}
